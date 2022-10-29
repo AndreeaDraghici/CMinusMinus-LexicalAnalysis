a8567da9d60755ed558adb5c605b0311686ed68c
Drăghici Andreea-Maria <draghici108@gmail.com>
2022-10-29 13:14:09 +0300

    updated main

--- src/implementation/Main.java
+++ src/implementation/Main.java
@@ -1,10 +1,13 @@
 package implementation;
 
+import static implementation.Utils.INPUT_FILE_TXT;
+
 public class Main {
+
     public static void main(String[] args) {
 
         try {
-            Scanner.lexicalAnalysis("src/resources/inputFile.txt");
+            Scanner.lexicalAnalysis(INPUT_FILE_TXT);
         } catch (Exception e) {
             System.out.println("Exceptie: " + e.getMessage());
         }
