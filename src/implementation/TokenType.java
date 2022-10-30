d714f3ee5e622cb457fe15333e01152559e22278
Drăghici Andreea-Maria <draghici108@gmail.com>
2022-10-30 12:03:25 +0200

    updated token types

--- src/implementation/TokenType.java
+++ src/implementation/TokenType.java
@@ -4,7 +4,7 @@ public class TokenType {
 
     // reprezinta lista de cuvinte rezervate
     public static String[] keyword = new String[]{"if", "else", "int",
-            "return", "void", "while", "true", "false", "cin", "cout"};
+            "return", "void", "while", "true", "false", "cin", "cout", "bool", "type"};
 
     //reprezinta lista de caractere/simboluri terminale
     public static char[] delimiter = new char[]{'(', ')', '*', '/',
